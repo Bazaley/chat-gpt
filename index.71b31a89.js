6e1d6ee0d3bffd7a2cc37b90509981b609119981
Bazaley <88235404+Bazaley@users.noreply.github.com>
2023-04-23 21:17:43 +0000

    Deploying to gh-pages from @ Bazaley/chat-gpt@d85ec53cac3127a45a226dff1399dbb00696a83e 🚀

---
+++ index.71b31a89.js
@@ -0,0 +1,2 @@
+document.querySelector(".questions__list").addEventListener("click",(e=>{e.target.parentNode.lastElementChild.classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.71b31a89.js.map
---
+++ index.a20a76e6.js
@@ -0,0 +1,2 @@
+document.querySelector(".questions__list").addEventListener("click",(e=>{e.target.parentNode.lastElementChild.classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.a20a76e6.js.map
